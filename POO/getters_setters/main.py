d8d069c7a8611a39ec2af97f0fd21cdcb0b541f9
Gustavo Bezerra <91147913+GustavoBezerraSE@users.noreply.github.com>
2022-05-31 08:20:30 -0300

    Add files via upload

---
+++ POO/getters_setters/main.py
@@ -0,0 +1,35 @@
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco      # 1 antes de passar por aqui primeiro vai passar la no setter
+
+    def desconto(self, percentual):
+        self.preco = self.preco - (self.preco * (percentual / 100))
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, valor):
+        self._nome = valor.title()
+
+    # Getter
+    @property
+    def preco(self):
+        return self._preco
+
+    #Setter
+    @preco.setter               # 2 quando passar aqui sera configurado o preço de fato como se fosse uma proteção
+    def preco(self, valor):
+        if isinstance(valor, str):
+            valor = float(valor.replace("R$", ""))
+        self._preco = valor
+
+p1 = Produto("CAMISETA", 50)
+p1.desconto(10)
+print(p1.nome, p1.preco)
+
+p2 = Produto("Caneta", "R$15")
+p2.desconto(10)
+print(p2.nome, p2.preco)
